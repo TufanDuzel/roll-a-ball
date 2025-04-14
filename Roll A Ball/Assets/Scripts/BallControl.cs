ea1809122fba8774faffdd9d54ebe20bacbe4fe3
Tufan Duzel <tufansahind@gmail.com>
2025-04-14 20:42:33 +0200

    roll-a-ball

---
+++ Roll A Ball/Assets/Scripts/BallControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BallControl : MonoBehaviour
+{
+    public int speed;
+    public Rigidbody physics;
+    public int point;
+    public int cubes;
+    public Text pointText;
+    public Text gameOverText;
+
+    void Start()
+    {
+        physics = GetComponent<Rigidbody>();
+    }
+
+    
+    void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 vector = new Vector3(horizontal, 0, vertical);
+        physics.AddForce(vector * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject);
+        other.gameObject.SetActive(false);
+
+        point += 1;
+        pointText.text = "Point: " + point;
+
+        if (point == cubes)
+        {
+            gameOverText.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Roll A Ball/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+    public GameObject ball;
+    public Vector3 distance;
+
+    void Start()
+    {
+        distance = transform.position - ball.transform.position;
+    }
+
+    
+    void LateUpdate()
+    {
+        transform.position = ball.transform.position + distance;
+    }
+}
